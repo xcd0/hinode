46739e1484ccefb55a1c35d5cf0393d760dd3d79
xcd0 <xcd0@xcd0.com>
2020-06-28 01:15:16 +0900

    左手の並びがおかしかったので修正

--- build_hinode.sh
+++ build_hinode.sh
@@ -11,12 +11,18 @@ cp -rf  $hinode_dir ../qmk_firmware/keyboards/
 
 cd ../qmk_firmware
 make hinode:default
-cp hinode*.hex ../hinode
+
+if [ ! $? ]; then
+	exit 1
+fi
+cp hinode_rev2_default.hex ../hinode
 
 echo ls -al ../qmk_firmware/keyboards/hinode
 ls -al ../qmk_firmware/keyboards/hinode
 
 if [ -e /mnt/c/home ]; then
+	echo rm -rf /mnt/c/home/work/hinode
+	rm -rf /mnt/c/home/work/hinode
 	echo cp -rf $SCRIPT_DIR /mnt/c/home/work/
 	cp -rf $SCRIPT_DIR /mnt/c/home/work/
 	echo ls -al /mnt/c/home/work/hinode
